b063dc9adbf0a780b5ecf7de8d667a13a2d74d5b
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-03-28 14:34:52 +0200

    Forgot to update relays

--- src/const.tsx
+++ src/const.tsx
@@ -1,13 +1,13 @@
 export let defaultRelaysToPublish = [
-  // "wss://nostr-pub.wellorder.net/",
-  // "wss://relay.nostr.scot",
-  "ws://localhost:8080",
+  "wss://nostr-pub.wellorder.net/",
+  "wss://relay.nostr.scot",
+  // "ws://localhost:8080",
 ];
 
 export let defaultRelays = [
-  // "wss://nos.lol",
+  "wss://nos.lol",
   "wss://relay.damus.io",
-  // "wss://arnostr.permadao.io",
-  // "wss://relay.snort.social",
-  "ws://localhost:8080",
+  "wss://arnostr.permadao.io",
+  "wss://relay.snort.social",
+  // "ws://localhost:8080",
 ];
